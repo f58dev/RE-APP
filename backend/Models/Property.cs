083fd02b8104529194853f62c2625e6f5891013c
ケイシー <192527421+f58dev@users.noreply.github.com>
2025-05-21 11:03:39 +0300

    ImageUrl

--- backend/Models/Property.cs
+++ backend/Models/Property.cs
@@ -1,10 +1,12 @@
 namespace backend.Models 
 {
-    public class Property {
-        public int Id {get; set;}
-        public string? Name {get; set;}
-        public string? Location {get; set;}
-        public decimal PricePerNight {get; set;}
-        public string? Description {get; set;}
+    public class Property
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Location { get; set; }
+        public decimal PricePerNight { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
     }
 }
